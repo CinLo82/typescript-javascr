c8ed1fd870db081ff583c5b035850386e2f3a4ab
CinLo82 <cinlos3382@gmail.com>
2024-07-21 01:15:23 -0300

    module

---
+++ javascript-moderno/12-modulos.js
@@ -0,0 +1,8 @@
+import { printArray, hello } from "./13-mi-modulo.js"
+
+console.log('archivo main de mi proyecto')
+
+let names = [ 'Paco', 'Pepe', 'Juan']
+printArray(names) 
+
+hello('Cinlo')
---
+++ javascript-moderno/13-mi-modulo.js
@@ -0,0 +1,12 @@
+export function printArray(datos) {
+    datos.forEach(element => {
+        console.log(element)
+    });
+    return datos
+}
+
+export function hello(name) {
+    console.log(name)
+
+    return name
+}
