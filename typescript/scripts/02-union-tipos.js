a23551ebb76aeeee04f50e5bfa07a9cabbb9762c
CinLo82 <cinlos3382@gmail.com>
2024-07-22 11:23:09 -0300

    union de datos y reorganizacion de archivos

---
+++ typescript/scripts/02-union-tipos.js
@@ -0,0 +1,4 @@
+"use strict";
+// union de tipos
+let contact = 656217321;
+contact = 'cinlo@cinlo.com';
---
+++ typescript/src/02-union-tipos.ts
@@ -0,0 +1,3 @@
+// union de tipos
+let contact: string | number = 656217321;
+contact = 'cinlo@cinlo.com'; 
