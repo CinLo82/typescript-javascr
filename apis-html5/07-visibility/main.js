bd6a7bc8a34fdf5bb5cdcf75ee79b7c012c4313f
CinLo82 <cinlos3382@gmail.com>
2024-07-24 00:32:59 -0300

    visibility change

---
+++ apis-html5/07-visibility/main.js
@@ -0,0 +1,10 @@
+let mivideo = document.querySelector('#mivideo')
+
+window.addEventListener('visibilitychange', () => {
+
+    if(document.visibilityState === 'visible') {
+        mivideo.play();
+    }else {
+        mivideo.pause();
+    }
+})
