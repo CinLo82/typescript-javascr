d643ce406663e7f958678377bd98115e8c1babb8
CinLo82 <cinlos3382@gmail.com>
2024-07-24 17:56:38 -0300

    configuración para js, app y load

---
+++ proyecto/js/main.js
@@ -0,0 +1,7 @@
+import App from './src/peli/app.js'
+
+// crear el objeto app
+const peli_app = new App()
+
+//cargar toda la app
+peli_app.load()
---
+++ proyecto/js/src/peli/app.js
@@ -0,0 +1,16 @@
+export default class App{
+
+    constructor(){
+
+    }
+
+    load(){
+        //añadir peliculas
+
+        //listar peliculas
+
+        // buscar peliculas
+
+        console.log('La applicación de películas ha sido inicializada')
+    }
+}
